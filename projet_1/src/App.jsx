984674cf7966a801faec967136c42d983e2d98bb
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-03-31 11:54:39 +0200

    enlevé style inline

--- projet_1/src/App.jsx
+++ projet_1/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"; // Import the CSS file
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -15,18 +16,16 @@ function App() {
     <Container className="mt-4 mx-auto">
       <Row className="justify-content-center">
         {products.map((product) => (
-          <Col key={product.id} md={4} lg={3}className="mb-4 d-flex justify-content-center">
+          <Col key={product.id} md={4} lg={3} className="mb-4 d-flex justify-content-center">
             <Card className="shadow-sm">
               <Card.Img
                 variant="top"
                 src={product.image}
-                style={{ height: "200px", objectFit: "contain" }}
+                className="card-img"
               />
-              <Card.Body className="d-flex flex-column">
+              <Card.Body className="card-body">
                 <Card.Title>{product.title}</Card.Title>
-                <Card.Text>
-                  {product.description}
-                </Card.Text>
+                <Card.Text>{product.description}</Card.Text>
                 {product.price}€
               </Card.Body>
             </Card>
