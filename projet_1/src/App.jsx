bd8411f3c35724b21aaa709c701e958990147965
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-03-31 15:52:33 +0200

    patch

--- projet_1/src/App.jsx
+++ projet_1/src/App.jsx
@@ -62,6 +62,25 @@ function App() {
     alert("Le produit avec l'id " + data.id + " a été modifié");
   };
 
+  const updateProductPrice = async (id) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ price: 39.99}),
+    });
+    const data = await response.json();
+
+    setProducts(
+      products.map((product) =>
+        product.id === id ? { ...product, price: 39.99 } : product
+      )
+    );
+
+    alert("Le prix du produit avec l'id " + data.id + " a été modifié");
+  };
+
   return (
     <Container className="mt-4 mx-auto">
       <Row className="justify-content-center mb-4">
@@ -95,6 +114,13 @@ function App() {
                   variant="warning"
                 >
                   Modifier le produit complet
+                </Button>
+                <Button
+                  onClick={() => updateProductPrice(product.id)}
+                  className="update-price-btn my-2"
+                  variant="success"
+                >
+                  Modifier le prix du produit
                 </Button>
               </Card.Body>
             </Card>
