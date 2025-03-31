e65bdc305fb585a4ff0dbab72dfd146e831172da
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-03-31 11:44:26 +0200

    first commit

---
+++ projet_1/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <Container className="mt-4 mx-auto">
+      <Row className="justify-content-center">
+        {products.map((product) => (
+          <Col key={product.id} md={4} lg={3}className="mb-4 d-flex justify-content-center">
+            <Card className="shadow-sm">
+              <Card.Img
+                variant="top"
+                src={product.image}
+                style={{ height: "200px", objectFit: "contain" }}
+              />
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>
+                  {product.description}
+                </Card.Text>
+                {product.price}€
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
